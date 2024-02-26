87f2d05d797eaa5df65639e1134804feb1a3c93b
samuelpd22 <samuel050101@hotmail.com>
2024-02-26 19:02:00 -0300

    Retulizando components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 
 import './App.css';
+import HelloWorld from './components/HelloWorld';
+
 
 function App() {
   const name= "Samuel";
@@ -11,13 +13,14 @@ function App() {
   }
 
   const url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHNo-lk0YLf0NOXf3MG3ZLaZi9dkY3a5o1AD065jj0a-bpLQpm4VqdPAgA04OzzmnzAUY&usqp=CAU"
+
   return (
     <div className="App">
-      <h1>Ola react</h1>
-      <p>Meu primeiro app</p>
       <p>Ola, {newName}</p>
       <p>Soma {sum(4,4)}</p>
       <img src={url} alt="Minha imagem"/>
+      <HelloWorld/>
+     
     </div>
   );
 }
---
+++ src/components/Frase.js
@@ -0,0 +1,8 @@
+function Frase(){
+    return (
+        <div>
+            <p>Este e um componente com uma frase</p>
+        </div>
+    )
+}
+export default Frase;
