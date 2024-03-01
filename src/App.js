14fe022d8cf00f09f17b7f878c6908b98d96a6ff
samuelpd22 <samuel050101@hotmail.com>
2024-03-01 17:39:44 -0300

    React Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-
-import { useState } from 'react';
+import { BrowserRouter as Router ,Routes ,Route , Link} from 'react-router-dom';
+import Home from './pages/components/Home';
+import Contato from './pages/components/Contato';
+import Empresa from './pages/components/Empresa';
 import './App.css';
-import Numerz from './components/Numerz';
+
 
 
 
 
 
 function App() {
-  const [num,setNum] = useState(1);
+  
   return (
-    <div className="App">
-      <h1>State lift 2</h1>
-      <Numerz num={num} setNum={setNum}/>
-      
-     
-     
-     
-      
-    </div>
+    <Router>
+    <ul>
+      <li><Link to='/'>Home</Link></li>
+      <li><Link to='/contato'>Contato</Link></li>
+      <li><Link to='/empresa'>Empresa</Link></li>
+    </ul>
+
+
+
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/contato" element={<Contato />} />
+              <Route path="/empresa" element={<Empresa />} />
+          </Routes>
+  </Router>
+    
   );
 }
 
---
+++ src/pages/components/Home.js
@@ -0,0 +1,11 @@
+
+function Home(){
+    return(
+        <div>
+            <h1>HOME</h1>
+            <p>Conteudo da pagina</p>
+        </div>
+    )
+
+}
+export default Home;
